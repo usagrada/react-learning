3c20763edd7612d6826478c6d0352fec998674fe
usagrada <usagrada.kind@gmail.com>
2021-09-06 01:17:04 +0900

    Fix Image loader

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ require('dotenv').config();
 module.exports = {
   reactStrictMode: true,
   assetPrefix: process.env.GITHUB_PAGES ? '/react-learning' : '',
+  images: {
+    loader: 'imgix',
+    path: '.',
+  },
 }
